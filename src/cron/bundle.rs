bea4a8bdd2ace0a0abc982a5688a6afe39684360
bluekirby1111 <blue.kirby@tutanota.com>
2022-01-24 21:05:14 -0300

    feat: validate L2 tx

--- src/cron/bundle.rs
+++ src/cron/bundle.rs
@@ -1,10 +1,17 @@
-
-use std::path::Path;
-
 use awc::Client;
-use bundlr_sdk::verify::{file::verify_file_bundle};
+use bundlr_sdk::JWK;
+use bundlr_sdk::deep_hash_sync::{ deep_hash_sync, ONE_AS_BUFFER };
+use bundlr_sdk::verify::types::Item;
+use bundlr_sdk::{ verify::file::verify_file_bundle, deep_hash::DeepHashChunk };
+use data_encoding::BASE64URL_NOPAD;
+use openssl::hash::MessageDigest;
+use openssl::pkey::{Public, PKey};
+use openssl::rsa::Padding;
+use openssl::sign;
 use paris::error;
 use serde::{Deserialize, Serialize};
+use lazy_static::lazy_static;
+use jsonwebkey::JsonWebKey;
 use crate::types::Validator;
 use crate::cron::arweave::arweave::Arweave;
 use super::error::ValidatorCronError;
@@ -50,7 +57,7 @@ pub async fn validate_bundler(bundler: Bundler) -> Result<(), ValidatorCronError
         for transaction in txs_req {
             // TODO: Check seeded [?]
             // TODO: Download bundle [?]
-            let tx = Tx {
+            let arweave_tx = Tx {
                 id: transaction.id.clone(),
                 block_height: match &transaction.block {
                     Some(b) => Some(b.height),
@@ -58,7 +65,7 @@ pub async fn validate_bundler(bundler: Bundler) -> Result<(), ValidatorCronError
                 }
             };
 
-            let file_path = arweave.get_tx_data(&tx.id).await;
+            let file_path = arweave.get_tx_data(&arweave_tx.id).await;
             if file_path.is_ok() {
                 println!("Verifying file: {}", &file_path.as_ref().unwrap());
                 let bundle_txs = match verify_file_bundle(file_path.unwrap()).await {
@@ -70,17 +77,16 @@ pub async fn validate_bundler(bundler: Bundler) -> Result<(), ValidatorCronError
                 };
                 
                 for bundle_tx in bundle_txs {
-                    let tx_receipt = if let Ok(tx_receipt) = tx_exists_in_db(tx.id.as_str()).await {
+                    let tx_receipt = if let Ok(tx_receipt) = tx_exists_in_db(&bundle_tx).await {
                         tx_receipt
-                    } else if let Ok(tx_receipt) = tx_exists_on_peers(tx.id.as_str()).await {
+                    } else if let Ok(tx_receipt) = tx_exists_on_peers(&bundle_tx.tx_id).await {
                         tx_receipt
                     } else {
                         continue;
                     };
     
-                    println!("Tx receipt: {:?}", &tx_receipt);
-                    // Verify tx receipt
-    
+                    let tx_is_ok = verify_tx_receipt(&tx_receipt);
+                    println!("{:?}", tx_is_ok);
                 }
             }
         }
@@ -97,8 +103,16 @@ pub async fn validate_bundler(bundler: Bundler) -> Result<(), ValidatorCronError
     Ok(())
 }
 
-async fn tx_exists_in_db(tx_id: &str) -> Result<TxReceipt, ValidatorCronError> {
-    Ok(TxReceipt::default())
+// TODO: implement the database verification correctly
+async fn tx_exists_in_db(bundle_tx: &Item) -> Result<TxReceipt, ValidatorCronError> {
+    Ok(TxReceipt { 
+        block: 10,
+        tx_id: bundle_tx.tx_id.clone(),
+        signature: match String::from_utf8(bundle_tx.signature.clone()) {
+            Ok(s) => s,
+            Err(_) => String::new(),
+        },
+    })
 }
 
 async fn tx_exists_on_peers(tx_id: &str) -> Result<TxReceipt, ValidatorCronError> {
@@ -126,4 +140,44 @@ async fn tx_exists_on_peers(tx_id: &str) -> Result<TxReceipt, ValidatorCronError
     }
 
     Err(ValidatorCronError::TxNotFound)
+}
+
+
+fn verify_tx_receipt(tx_receipt: &TxReceipt) -> std::io::Result<bool> {
+    pub const BUNDLR_AS_BUFFER: &[u8] = "Bundlr".as_bytes();
+
+    let block = tx_receipt.block.to_string()
+        .as_bytes()
+        .to_vec();
+
+    let tx_id = tx_receipt.tx_id.as_bytes().to_vec();
+
+    let message = deep_hash_sync(DeepHashChunk::Chunks(vec![
+        DeepHashChunk::Chunk(BUNDLR_AS_BUFFER.into()),
+        DeepHashChunk::Chunk(ONE_AS_BUFFER.into()),
+        DeepHashChunk::Chunk(tx_id.into()),
+        DeepHashChunk::Chunk(block.into())
+    ])).unwrap();
+
+    lazy_static! {
+        static ref PUBLIC: PKey<Public> = {
+            let jwk = JWK {
+                kty: "RSA",
+                e: "AQAB",
+                n: std::env::var("BUNDLER_PUBLIC").unwrap()
+            };
+
+            let p = serde_json::to_string(&jwk).unwrap();
+            let key: JsonWebKey = p.parse().unwrap();
+            
+            PKey::public_key_from_der(key.key.to_der().as_slice()).unwrap()
+        };
+    };
+
+    let sig = BASE64URL_NOPAD.decode(tx_receipt.signature.as_bytes()).unwrap();
+
+    let mut verifier = sign::Verifier::new(MessageDigest::sha256(), &PUBLIC).unwrap();
+    verifier.set_rsa_padding(Padding::PKCS1_PSS).unwrap();
+    verifier.update(&message).unwrap();
+    Ok(verifier.verify(&sig).unwrap_or(false))
 }
