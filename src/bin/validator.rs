ff35a2df3a7348d40f43d943e90bd47e0c6e50fb
Josh Benaron <joshbenaron@gmail.com>
2022-07-09 13:33:08 +0100

    Use async-trait to convert cli opts to context

--- src/bin/validator.rs
+++ src/bin/validator.rs
@@ -53,10 +53,13 @@ struct CliOpts {
     #[clap(long, env = "ARWEAVE_URL")]
     arweave_url: Option<Url>,
 
+    #[clap(long)]
+    bundler_key: Option<Url>,
+
     #[clap(
         long,
         env = "CONTRACT_GATEWAY",
-        default_value = "https://contract-gateway.testnet1.bundlr.network"
+        default_value = "http://localhost:3000"
     )]
     contract_gateway_url: Url,
 }
@@ -105,73 +108,84 @@ struct PublicResponse {
     n: String,
 }
 
+#[async_trait::async_trait]
+pub trait IntoAsync<T> {
+    async fn into_async(&self) -> T;
+}
+
 // TODO: This does not belong here, create a new time for AppContextConfig and move to context module
-impl From<&CliOpts> for AppContext {
-    fn from(config: &CliOpts) -> Self {
-        let n_response = reqwest::blocking::get(format!("{}/public", &config.bundler_url))
+
+#[async_trait::async_trait]
+impl IntoAsync<AppContext> for CliOpts {
+    async fn into_async(&self) -> AppContext {
+        let n_response = reqwest::get(format!("{}/public", &self.bundler_url))
+            .await
             .expect("Couldn't get public key from bundler")
             .json::<PublicResponse>()
+            .await
             .expect("Couldn't parse public key response from bundler");
 
         let bundler_jwk =
             public_only_jwk_from_rsa_n(&n_response.n).expect("Failed to decode bundler key");
 
         let validator_jwk: JsonWebKey = {
-            let file = fs::read_to_string(&config.validator_key).unwrap();
+            let file = fs::read_to_string(&self.validator_key).unwrap();
             file.parse().unwrap()
         };
 
         let key_manager = InMemoryKeyManager::new(&Keys(bundler_jwk, validator_jwk));
         let state = generate_state();
 
-        let connection_mgr = ConnectionManager::<SqliteConnection>::new(&config.database_url);
+        let connection_mgr = ConnectionManager::<SqliteConnection>::new(&self.database_url);
         let pool = r2d2::Pool::builder()
             .build(connection_mgr)
             .expect("Failed to create SQLite connection pool.");
 
-        if &config.database_url == ":memory:" {
+        if &self.database_url == ":memory:" {
             embedded_migrations::run(&pool.get().unwrap()).unwrap();
         }
 
-        let arweave_url = match &config.arweave_url {
+        let arweave_url = match &self.arweave_url {
             Some(url) => url,
             None => unreachable!(),
         };
 
-        Self::new(
+        AppContext::new(
             key_manager,
             pool,
-            config.listen,
+            self.listen,
             state,
             reqwest::Client::new(),
             arweave_url,
-            &config.bundler_url,
-            &config.contract_gateway_url,
+            &self.bundler_url,
+            &self.contract_gateway_url,
         )
     }
 }
 
-#[actix_web::main]
-async fn main() -> () {
-    dotenv::dotenv().ok();
 
-    env_logger::init_from_env(Env::default().default_filter_or("info"));
+fn main() -> () {
+    actix_rt::System::new().block_on(async {
+        dotenv::dotenv().ok();
 
-    let http_client = ReqwestClient::new(reqwest::Client::new());
-    let app_config = CliOpts::parse();
-    let bundler_config = BundlerConfig::fetch_config(http_client, &app_config.bundler_url).await;
-    let config = merge_configs(app_config, bundler_config);
-    let ctx = AppContext::from(&config);
-
-    if !config.no_cron {
-        paris::info!("Running with cron");
-        tokio::task::spawn_local(run_crons(ctx.clone()));
-    };
-
-    if !config.no_server {
-        paris::info!("Running with server");
-        run_server(ctx.clone()).await.unwrap()
-    };
+        env_logger::init_from_env(Env::default().default_filter_or("info"));
+    
+        let http_client = ReqwestClient::new(reqwest::Client::new());
+        let app_config = CliOpts::parse();
+        let bundler_config = BundlerConfig::fetch_config(http_client, &app_config.bundler_url).await;
+        let config = merge_configs(app_config, bundler_config);
+        let ctx = config.into_async().await;
+    
+        if !config.no_cron {
+            paris::info!("Running with cron");
+            tokio::task::spawn_local(run_crons(ctx.clone()));
+        };
+    
+        if !config.no_server {
+            paris::info!("Running with server");
+            run_server(ctx.clone()).await.unwrap()
+        };
+    });
 }
 
 #[cfg(test)]
