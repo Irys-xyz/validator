2265df5e480103a0eccb4a5004eddcfc061a4092
JesseTheRobot <jesse.cruz.wright@gmail.com>
2022-08-03 15:47:15 +0100

    fix: :bug: contracts-rust -> contracts for ecosystem file
    Updates the PM2 ecosystem file to reflect the recent naming changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,7 +12,7 @@ module.exports = {
         },
         {
             "name": "Contract Gateway",
-            "script": "./contracts-rust/gateway/dist/gateway/src/index.js"
+            "script": "./contracts/gateway/dist/gateway/src/index.js"
         }
     ]
 }
